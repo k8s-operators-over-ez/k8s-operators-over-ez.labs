c93227b8138b1fdf378b3b8152a7abc157ad453d
hershey <hershey@hershey.lan>
2020-12-17 17:10:01 -0600

    helm op in progress

---
+++ helm-operator-labs/helm-charts/helmopsovereasy/scripts/test.sh
@@ -0,0 +1,1 @@
+echo "hello world" && sleep 30
---
+++ helm-operator-labs/scripts/test.sh
@@ -0,0 +1,13 @@
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: busybox-sleep-less
+spec:
+  containers:
+  - name: busybox
+    image: busybox
+    args:
+    - sleep
+    - "1000"
+EOF
